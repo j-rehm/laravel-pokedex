47a17a21ac5b6292c6626a8ec1212ae6780ac6fe
DeryGermann <termaiter@gmail.com>
2020-08-26 18:27:24 -0600

    stuff

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,15 @@ Route::get('/team', function () {
     ]);
 })->name('team');
 Route::post('/team', 'TeamController@toggle');
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+Route::post('/signup', 'AccountController@SignUp');
+Route::get('/login', function () {
+    $trainer = DB::table('trainer')
+               ->get();
+    return view('login', [
+        'trainer' => $trainer
+    ]);
+})->name('login');
